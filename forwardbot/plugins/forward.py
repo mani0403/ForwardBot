c8398b173a691997e9b7f87f13ffff7b512b28f6
mani0403 <80608269+mani0403@users.noreply.github.com>
2021-07-11 23:45:54 +0530

    Update forward.py

--- forwardbot/plugins/forward.py
+++ forwardbot/plugins/forward.py
@@ -157,7 +157,7 @@ async def handler(event):
                         if media_type(message) == type or type == 'All':
                             try:
                                 if media_type(message) == 'Document':
-                                    await client.send_file(tochat, message.document , captions)
+                                    await client.send_file(tochat, message.document , caption)
                                     try:
                                         if len(str(message.file.name)) <= 95:
                                             print("Succesfully forwarded: " + str(message.file.name))
